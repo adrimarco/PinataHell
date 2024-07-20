1fec759349267c54ad330d7f973a26f3b62f36f6
adrimarco <adrianmarco-@hotmail.es>
2024-07-20 12:40:56 +0200

    feat: add difficulty progression

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -12,8 +13,12 @@ public class SpawnManager : MonoBehaviour
     float spawnTime = 0.0f;
     [SerializeField] float maxSpawnTime = 10.0f;
 
+    // Simultaneous enemies counter
     int enemiesCount = 0;
-    [SerializeField] int maxEnemiesCount = 20;
+    int maxEnemiesCount = 20;
+    // Enemies that appear at each round counter
+    int roundEnemiesCount = 0;
+    int maxRoundEnemiesCount = 20;
 
     Transform playerTransform;
     List<Transform> spawnPoints;
@@ -22,10 +27,34 @@ public class SpawnManager : MonoBehaviour
     float generateNewDistancesTime = 0.0f;
     [SerializeField] float maxGenerateNewDistancesTime = 3.0f;
 
+    // Rounds stats
+    [Space(4)]
+    [Header("Round progression")]
+    public short initialEnemies = 4;
+    public short enemiesIncrementPerRound = 4;
+    public float forceNewRoundTime = 20;
+    [Space(2)]
+    public float baseEnemyHealth = 5;
+    public float enemyHealthIncrementPerRound = 2;
+    [Space(2)]
+    public float baseEnemyDamage = 10;
+    public float enemyDamageIncrementPerRound = 3;
+
+    private int round = 1;
+    private float enemyDamage = 1;
+    private float enemyHealth = 1;
+    private float newRoundTimer = 20;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        round = 1;
+        maxRoundEnemiesCount = initialEnemies;
+        enemyDamage = baseEnemyDamage;
+        enemyHealth = baseEnemyHealth;
+
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         spawnPoints = new List<Transform>(GameObject.Find("SpawnPointsContainer").GetComponentsInChildren<Transform>());
         spawnPoints.RemoveAt(0);
@@ -33,7 +62,7 @@ public class SpawnManager : MonoBehaviour
         for (int i = 0; i < spawnPoints.Count; i++)
         {
             Vector3 pos = spawnPoints[i].position;
-            spawnPointsDistances.Add(Mathf.Pow(pos.x + playerTransform.position.x, 2) - Mathf.Pow(pos.y + playerTransform.position.y, 2) - Mathf.Pow(pos.z + playerTransform.position.z, 2));
+            spawnPointsDistances.Add(Mathf.Pow(pos.x - playerTransform.position.x, 2) + Mathf.Pow(pos.y - playerTransform.position.y, 2) + Mathf.Pow(pos.z - playerTransform.position.z, 2));
         }
 
         Enemy.onEnemyDead.AddListener(OnEnemyDeath);
@@ -42,15 +71,44 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (generateNewDistancesTime > 0.0f) generateNewDistancesTime -= Time.fixedDeltaTime;
+        if (generateNewDistancesTime > 0.0f) generateNewDistancesTime -= Time.deltaTime;
+
+        // When all enemies are dead or have past some time since last spawn, start new round
+        if (roundEnemiesCount >= maxRoundEnemiesCount)
+        {
+            newRoundTimer -= Time.deltaTime;
+            if (newRoundTimer <= 0) NewRound();
+        }
 
         if (spawnTime > 0.0f)
-            spawnTime -= Time.fixedDeltaTime;
-        else if(enemiesCount < maxEnemiesCount) spawnEnemyOnRandomSpawnPoint();
+            spawnTime -= Time.deltaTime;
+        else if (enemiesCount < maxEnemiesCount && roundEnemiesCount < maxRoundEnemiesCount)
+        {
+            spawnEnemyOnRandomSpawnPoint();
+        }
     }
 
 
     void spawnEnemyOnRandomSpawnPoint()
+    {
+        GameObject newEnemy = Instantiate(spawnPrefab, GetRandomSpawnPosition(), Quaternion.identity);
+
+        Enemy e = newEnemy.GetComponentInChildren<Enemy>();
+        if (e != null)
+        {
+            e.healthComp.health = enemyHealth;
+            e.attackDamage = enemyDamage;
+        }
+
+        spawnTime = maxSpawnTime;
+        enemiesCount++;
+        roundEnemiesCount++;
+
+        // When all enemies of the round have spawned, start the timer for the next round
+        if (roundEnemiesCount >= maxRoundEnemiesCount) newRoundTimer = forceNewRoundTime;
+    }
+
+    private Vector3 GetRandomSpawnPosition()
     {
         if (generateNewDistancesTime <= 0.0f)
         {
@@ -60,21 +118,21 @@ public class SpawnManager : MonoBehaviour
         }
 
         List<Transform> poolSpawnPoints = new List<Transform>();
-        for (int i = 0; i < spawnPoints.Count; i++) {
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
             if (Math.Abs(spawnPointsDistances[i]) < maxSpawnDistanceFromPlayer)
             {
                 poolSpawnPoints.Add(spawnPoints[i]);
-            } else
+            }
+            else
             {
                 break;
             }
         }
-        Debug.Log(poolSpawnPoints.Count);
 
         if (poolSpawnPoints.Count > 0)
         {
-
-            bool enemySpawned = false;
+            bool positionFound = false;
             for (int i = 0; i < maxTryPoints; i++)
             {
                 int randomPos = UnityEngine.Random.Range(0, poolSpawnPoints.Count - 1);
@@ -82,28 +140,21 @@ public class SpawnManager : MonoBehaviour
                 RaycastHit hit;
                 if (Physics.Raycast(lineOfSight, out hit))
                 {
-                    Debug.Log(hit.collider);
                     if (hit.collider != null)
                     {
-                        Instantiate(spawnPrefab, poolSpawnPoints[randomPos].position, Quaternion.identity);
-
-                        spawnTime = maxSpawnTime;
-                        enemiesCount++;
-                        enemySpawned = true;
-                        break;
+                        return poolSpawnPoints[randomPos].position;
                     }
                 }
             }
 
             // In case that the enemy was not spawned in maxTryPoints trys, spawn the enemy at the closest spawnpoint
-            if (!enemySpawned)
+            if (!positionFound)
             {
-                Instantiate(spawnPrefab, poolSpawnPoints[0].position, Quaternion.identity);
-
-                spawnTime = maxSpawnTime;
-                enemiesCount++;
+                return poolSpawnPoints[0].position;
             }
         }
+
+        return Vector3.zero;
     }
 
     void sortSpawnPointsByDistance()
@@ -114,16 +165,14 @@ public class SpawnManager : MonoBehaviour
         bool swapped;
         for (int i = 0; i < spawnPoints.Count - 1; i++)
         {
-           
-            
             swapped = false;
             for (int j = 0; j < spawnPoints.Count - i - 1; j++)
             {
                 
                 Vector3 spawnPosition = spawnPoints[j].position;
                 Vector3 nextSpawnPosition = spawnPoints[j+1].position;
-                float distance = Mathf.Pow(spawnPosition.x + playerPosition.x, 2) - Mathf.Pow(spawnPosition.y + playerPosition.y, 2) - Mathf.Pow(spawnPosition.z + playerPosition.z, 2);
-                float nextDistance = Mathf.Pow(nextSpawnPosition.x + playerPosition.x, 2) - Mathf.Pow(nextSpawnPosition.y + playerPosition.y, 2) - Mathf.Pow(nextSpawnPosition.z + playerPosition.z, 2);
+                float distance = Mathf.Pow(spawnPosition.x - playerPosition.x, 2) + Mathf.Pow(spawnPosition.y - playerPosition.y, 2) + Mathf.Pow(spawnPosition.z - playerPosition.z, 2);
+                float nextDistance = Mathf.Pow(nextSpawnPosition.x - playerPosition.x, 2) + Mathf.Pow(nextSpawnPosition.y - playerPosition.y, 2) + Mathf.Pow(nextSpawnPosition.z - playerPosition.z, 2);
 
                 if (distance > nextDistance)
                 {
@@ -143,5 +192,20 @@ public class SpawnManager : MonoBehaviour
     private void OnEnemyDeath(int _candies)
     {
         enemiesCount--;
+
+        // If all enemies are dead, start the next round in a short time
+        if (enemiesCount == 0 && roundEnemiesCount >= maxRoundEnemiesCount)
+        {
+            newRoundTimer = 5;
+        }
+    }
+
+    private void NewRound()
+    {
+        round += 1;
+        roundEnemiesCount = 0;
+        maxRoundEnemiesCount += enemiesIncrementPerRound;
+        enemyHealth += enemyHealthIncrementPerRound;
+        enemyDamage += enemyDamageIncrementPerRound;
     }
 }
